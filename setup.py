45eb94640cb4d64d0b7d8336ae28f64339751e92
Etienne Millon <me@emillon.org>
2012-09-28 15:03:22 +0200

    Add setup.py & travis.yml

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup (
+    name='hnmail',
+    version='0.1.0',
+    author='Etienne Millon',
+    author_email='me@emillon.org',
+    url='http://github.com/emillon/hnmail',
+    description='A mail gateway for hacker news'
+    )
