e2d053adad9ad81464eabff2b4830ed85fba6868
Etienne Millon <me@emillon.org>
2012-10-11 16:23:41 +0200

    move treeapi to a class variable

--- hnmail/tests.py
+++ hnmail/tests.py
@@ -5,53 +5,52 @@ import hnmail
 
 from mock import ListMDA, Message, TreeAPI
 
-def run(api):
-    """
-    Build a MDA, run tests with given TreeAPI and return delivered messages.
-    """
-    mda = ListMDA()
-    hnmail.run(network=api, mda=mda, quiet=True)
-    return mda.msgs
-
 class TestHN(unittest.TestCase):
+    def run_hnmail(self):
+        """
+        Build a MDA, run tests with given TreeAPI and return delivered messages.
+        """
+        mda = ListMDA()
+        hnmail.run(network=self.api, mda=mda, quiet=True)
+        return mda.msgs
+
+    def setUp(self):
+        self.api = TreeAPI()
+
     def test_get_empty(self):
-        msgs = run(TreeAPI())
+        msgs = self.run_hnmail()
         self.assertEquals(msgs, [])
 
     def test_one_message(self):
-        t = TreeAPI()
         msg = Message(url='example.com', title='an example')
-        t.add_disc(msg)
-        msgs = run(t)
+        self.api.add_disc(msg)
+        msgs = self.run_hnmail()
         self.assertEquals(len(msgs), 1)
 
     def test_comment(self):
-        t = TreeAPI()
         msg1 = Message(url='example.com', title='an example')
-        id1 = t.add_disc(msg1)
+        id1 = self.api.add_disc(msg1)
         msg2 = Message(text='A comment')
-        id2 = t.add_to(id1, msg2)
-        msgs = run(t)
+        id2 = self.api.add_to(id1, msg2)
+        msgs = self.run_hnmail()
         self.assertEquals(len(msgs), 2)
 
     def test_limit(self):
-        t = TreeAPI()
-        root = t.add_disc(Message(url='example.com', title='an example'))
+        root = self.api.add_disc(Message(url='example.com', title='an example'))
         n = 20
         for i in xrange(0, n):
             msg = Message(text='A comment')
-            t.add_to(root, msg)
-        msgs = t.search({'limit': 10})
+            self.api.add_to(root, msg)
+        msgs = self.api.search({'limit': 10})
         self.assertEquals(len(msgs['results']), 10)
 
     def test_wide_disc(self):
-        t = TreeAPI()
-        root = t.add_disc(Message(url='example.com', title='an example'))
+        root = self.api.add_disc(Message(url='example.com', title='an example'))
         n = 20
         for i in xrange(0, n):
             msg = Message(text='A comment')
-            t.add_to(root, msg)
-        msgs = run(t)
+            self.api.add_to(root, msg)
+        msgs = self.run_hnmail()
         self.assertEquals(len(msgs), 1+n)
 
 if __name__ == '__main__':
